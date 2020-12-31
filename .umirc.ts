2f1937ba257ff1a52d920fee22513c3a99ab4786
zc <z304307393@163.com>
2020-12-31 12:14:39 +0800

    add docs

--- .umirc.ts
+++ .umirc.ts
@@ -5,9 +5,19 @@ export default defineConfig({
   base: '/',
   mode: 'site',
   title: 'zc-react-comp',
-  favicon: '/images/React.svg',
-  logo: '/images/React.svg',
+  favicon: './images/React.svg',
+  logo: './images/React.svg',
   outputPath: 'docs-dist',
+  navs: {
+    'en-US': [
+      { title: "component", path: '/component' },
+      { title: 'github', path: 'https://github.com/z5829984520/zc-react-comp' }
+    ],
+    'zh-CN': [
+      { title: "组件", path: '/zh-CN/component' },
+      { title: 'github', path: 'https://github.com/z5829984520/zc-react-comp' }
+    ]
+  },
   history: {
     type: 'hash',
   },
